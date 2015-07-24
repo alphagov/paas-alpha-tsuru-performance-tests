f287e432bf86278a17a228a7469fc632da78aaa1
Hector Rivas Gandara <keymon@gmail.com>
2015-07-24 11:33:51 +0100

    Create a ssh wrapper per created user
    Each user will have a unique SSH key, so we need a unique ssh wrapper
    per user.

--- bulk_deploy/tsuru_api_service.rb
+++ bulk_deploy/tsuru_api_service.rb
@@ -90,7 +90,7 @@ class TsuruAPIService
     ssh_id_rsa_path = File.join(@tsuru_home, '.ssh', "id_rsa_#{user[:email]}")
     ssh_id_rsa_pub_path = File.join(@tsuru_home, '.ssh', "id_rsa_#{user[:email]}.pub")
     ssh_config_file = File.join(@tsuru_home, '.ssh', 'config')
-    ssh_wrapper_path = File.join(@tsuru_home, 'ssh-wrapper')
+    ssh_wrapper_path = File.join(@tsuru_home, "#{user[:email]}-ssh-wrapper")
 
     # Generate a new ssh key
     SshHelper.generate_key(ssh_id_rsa_path)
@@ -113,6 +113,8 @@ class TsuruAPIService
     new_api_client.add_key(public_key)
 
     user[:key] = ssh_id_rsa_path
+    user[:ssh_wrapper] = ssh_wrapper_path
+    user
   end
 
 end
