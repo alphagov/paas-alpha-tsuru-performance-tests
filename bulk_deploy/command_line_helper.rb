9a71bc6e1ab71559fd5f56d7f042f7f68b09f1a8
Hector Rivas Gandara <keymon@gmail.com>
2015-07-27 16:25:08 +0100

    Write to an optional file when running commands
    Allow the `command_line_helper` to receive an optional `output_file`
    with a file descriptor to write the output.
    
    This will allow pass files to capture the output in concurrent runs.

--- bulk_deploy/command_line_helper.rb
+++ bulk_deploy/command_line_helper.rb
@@ -3,10 +3,12 @@ require 'open3'
 # Generic wrapper around the CommandLine
 class CommandLineHelper
   attr_reader :exit_status, :stderr, :stdout, :stdouterr, :last_command, :env
+  attr_accessor :output_file
 
   def initialize(env = {}, options = {})
     @env = env
     @options = options
+    @output_file = options[:output_file]
   end
 
   def wait()
@@ -30,31 +32,46 @@ class CommandLineHelper
 
   protected
 
-  def execute_helper_async(*cmd)
+  def clear_run()
     @exit_status=nil
     @stderr = ''
     @stdout = ''
     @stdouterr = ''
+  end
 
-    $stdout.puts "Executing: #{@env.map { |k,v| "#{k}='#{v}'" }.join(' ')} #{cmd.join(' ')}" if @options[:verbose]
+  def write_message(msg)
+    $stdout.puts msg if @options[:verbose]
+    @output_file.puts msg if (@output_file and not @output_file.closed?)
+  end
+
+  def write_stdout(l)
+    $stdout.puts "stdout: #{l}" if @options[:verbose]
+    @output_file.puts "stdout: #{l}" if (@output_file and not @output_file.closed?)
+    @stdouterr << "stdout: #{l}"
+    @stdout << l
+  end
+
+  def write_stderr(l)
+    $stderr.puts "stderr: #{l}" if @options[:verbose]
+    @output_file.puts "stderr: #{l}" if (@output_file and not @output_file.closed?)
+    @stdouterr << "stderr: #{l}"
+    @stderr << l
+  end
+
+  def execute_helper_async(*cmd)
+    clear_run()
+    executing_log = "Executing: #{@env.map { |k,v| "#{k}='#{v}'" }.join(' ')} #{cmd.join(' ')}"
+    write_message(executing_log)
 
     @in_fd, @out_fd, @err_fd, @wait_thread = Open3.popen3(@env, *cmd)
     @in_fd.close if @options[:close_stdin]
 
     # Print standard out end error as they receive content
     @tout = Thread.new do
-      @out_fd.each {|l|
-        $stdout.puts "stdout: #{l}" if @options[:verbose]
-        @stdouterr << "stdout: #{l}"
-        @stdout << l
-      }
+      @out_fd.each {|l| write_stdout(l) }
     end
     @terr = Thread.new do
-      @err_fd.each {|l|
-        $stderr.puts "stderr: #{l}" if @options[:verbose]
-        @stdouterr << "stderr: #{l}"
-        @stderr << l
-      }
+      @err_fd.each {|l| write_stderr(l) }
     end
   end
 
@@ -66,7 +83,7 @@ class CommandLineHelper
 
     self.wait
 
-    $stdout.puts "Exit code: #{@exit_status}" if @options[:verbose]
+    write_message "Exit code: #{@exit_status}" if @options[:verbose]
     return @exit_status == 0
   end
 
